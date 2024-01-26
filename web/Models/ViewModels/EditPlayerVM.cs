2ec5e13bf2d607598f91e821f42da44e727d49bb
HTFDT <almetov_tag@mail.ru>
2024-01-26 18:48:03 +0500

    added edit player page

---
+++ web/Models/ViewModels/EditPlayerVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace web.Models.ViewModels;
+
+public class EditPlayerVM
+{
+    [DisplayName("Фамилия")]
+    public string LastName { get; set; }
+    [DisplayName("Имя")]
+    public string FirstName { get; set; }
+    [DisplayName("Пол")]
+    public Gender Gender { get; set; }
+    [DisplayName("Дата рождения")]
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
+    [DisplayName("Страна")]
+    public Country Country { get; set; }
+    [DisplayName("Команда")]
+    public Guid TeamID { get; set; }
+}
---
+++ web/Pages/Players/Edit.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using web.Data;
+using web.Models;
+using web.Models.ViewModels;
+
+namespace web.Pages.Players;
+
+public class EditModel : PageModel
+{
+    private IRepository<Player> _playersRepository;
+    private IRepository<Team> _teamsRepository;
+    
+    public EditModel(IRepository<Player> playersRepo, IRepository<Team> teamsRepo)
+    {
+        _playersRepository = playersRepo;
+        _teamsRepository = teamsRepo;
+    }
+    [BindProperty] public EditPlayerVM ViewModel { get; set; } = new();
+    public async Task<IActionResult> OnGetAsync(Guid? id)
+    {
+        if (id is null)
+            return NotFound();
+
+        var player = await _playersRepository.FindById(id.Value);
+        if (player is null)
+            return NotFound();
+
+        ViewModel = new EditPlayerVM
+        {
+            FirstName = player.FirstName,
+            LastName = player.LastName,
+            Country = player.Country,
+            DateOfBirth = player.DateOfBirth,
+            Gender = player.Gender,
+            TeamID = player.TeamID
+        };
+
+        ViewData["existingTeams"] = await _teamsRepository.GetAll();
+        
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(Guid id)
+    {
+        if (!ModelState.IsValid)
+        {
+            ViewData["existingTeams"] = await _teamsRepository.GetAll();
+            return Page();
+        }
+        
+        var player = await _playersRepository.FindById(id, true);
+        if (player is null)
+            return NotFound();
+        player.FirstName = ViewModel.FirstName;
+        player.LastName = ViewModel.LastName;
+        player.Country = ViewModel.Country;
+        player.DateOfBirth = ViewModel.DateOfBirth;
+        player.Gender = ViewModel.Gender;
+        player.TeamID = ViewModel.TeamID;
+        await _playersRepository.SaveChangesAsync();
+        return RedirectToPage("./Index");
+    }
+}
