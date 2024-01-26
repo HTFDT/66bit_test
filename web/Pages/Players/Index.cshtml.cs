a985f8d8e5cfa089638403c31d3da3c947dc94c1
HTFDT <almetov_tag@mail.ru>
2024-01-26 18:46:20 +0500

    added index page

---
+++ web/Pages/Players/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using web.Data;
+using web.Models;
+
+namespace web.Pages.Players;
+
+public class IndexModel : PageModel
+{
+    private IRepository<Player> _repository;
+    
+    public IndexModel(IRepository<Player> repo)
+    {
+        _repository = repo;
+    }
+
+    public IList<Player> Players { get; set; } = new List<Player>();
+    
+    public async Task OnGetAsync()
+    {
+        Players = await _repository.Query().AsNoTracking().Include(e => e.Team).ToListAsync();
+    }
+}
